0e61fcb6e84e0307dad4424e7a447b8f05695a9e
Hayden50 <haydenrieder50@hotmail.com>
2024-04-02 14:03:30 -0400

    Set up basic organization for ttt game

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
+import TicTacToe from "./components/tic-tac-toe/tic-tac-toe";
 
 function App() {
   return (
     <>
-      <div>
-        <p>This will be the landing page for the application</p>
-      </div>
-      <button>Game 1</button>
-      <button>Game 2</button>
+      <TicTacToe />
     </>
   );
 }
---
+++ src/components/tic-tac-toe/tic-tac-toe.jsx
@@ -0,0 +1,11 @@
+import "./tic-tac-toe.css";
+
+const TicTacToe = () => {
+  return (
+    <div className="test">
+      <h1> Hello </h1>
+    </div>
+  );
+};
+
+export default TicTacToe;

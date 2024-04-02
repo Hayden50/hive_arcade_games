beb11738c9387aa4bf463b212adfa058f9e25728
Hayden50 <haydenrieder50@hotmail.com>
2024-04-02 14:13:53 -0400

    Update naming conventions and play around with CSS grid

---
+++ src/components/tic-tac-toe/ticTacToe.jsx
@@ -0,0 +1,23 @@
+import "./tic-tac-toe.css";
+
+const TicTacToe = () => {
+  return (
+    <div className="grid-container">
+      <div className="div1">
+        <button>
+          <p> Hello </p>
+        </button>
+      </div>
+      <div className="div2"> 2 </div>
+      <div className="div3"> 3 </div>
+      <div className="div4"> 4 </div>
+      <div className="div5"> 5 </div>
+      <div className="div6"> 6 </div>
+      <div className="div7"> 7 </div>
+      <div className="div8"> 8 </div>
+      <div className="div9"> 9 </div>
+    </div>
+  );
+};
+
+export default TicTacToe;

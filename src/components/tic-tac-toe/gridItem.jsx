f4dfb7dcc0c91e1d8164143e09dec65da0aa2894
Hayden50 <haydenrieder50@hotmail.com>
2024-04-03 13:57:07 -0400

    Updated state settings in GridItem

--- src/components/tic-tac-toe/gridItem.jsx
+++ src/components/tic-tac-toe/gridItem.jsx
@@ -1,7 +1,22 @@
 import "./ticTacToe.css";
+import { useState } from "react";
 
 const GridItem = (props) => {
-  return <button className="selector-button">{props.id}</button>;
+  const [clickable, setClickable] = useState(true);
+  const [value, setValue] = useState(null);
+
+  const handleClick = () => {
+    if (clickable) {
+      setClickable(false);
+      setValue("x");
+    }
+  };
+
+  return (
+    <button className="selector-button" onClick={handleClick}>
+      <h1 className="marking">{value}</h1>
+    </button>
+  );
 };
 
 export default GridItem;

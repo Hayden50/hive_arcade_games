0f7794a58b32f712082d6ad44282980a8414a9fc
Hayden50 <haydenrieder50@hotmail.com>
2024-04-21 16:26:54 -0400

    Mostly working verstion of tic-tac-toe. Still need networking for completion

--- src/components/tic-tac-toe/TicTacToe.jsx
+++ src/components/tic-tac-toe/TicTacToe.jsx
@@ -4,23 +4,40 @@ import "./ticTacToe.css";
 import { useEffect, useState } from "react";
 
 const TicTacToe = () => {
-  const [myTurn, updateMyTurn] = useState(false);
+  const initArr = Array.from({ length: 3 }, () => Array(3).fill(0));
+  const [board, setBoard] = useState(initArr);
+  const [myTurn, updateMyTurn] = useState(true);
   const [amX, setAmX] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const initialSetup = () => {
+    setAmX(Math.random() < 0.5);
+    // updateMyTurn(amX); // X is the first player to move
+  };
 
   useEffect(() => {
-    setAmX(Math.random() < 0.5);
-    // updateMyTurn(amX);
-    console.log("AM X: ", amX);
+    initialSetup();
   });
 
   return (
-    <Grid container spacing={2} sx={borderStyling}>
-      {[...Array(9)].map((_, index) => (
-        <Grid key={index} xs={4} minHeight={160}>
-          <GridItem id={index} amX={amX}></GridItem>
-        </Grid>
-      ))}
-    </Grid>
+    <div>
+      <h1>{message}</h1>
+      <Grid container spacing={2} sx={borderStyling}>
+        {[...Array(9)].map((_, index) => (
+          <Grid key={index} xs={4} minHeight={160}>
+            <GridItem
+              id={index}
+              amX={amX}
+              myTurn={myTurn}
+              setBoard={setBoard}
+              board={board}
+              updateMyTurn={updateMyTurn}
+              setMessage={setMessage}
+            ></GridItem>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 };
 

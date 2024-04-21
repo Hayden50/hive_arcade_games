5cb5214c3b5eac719b3b9d758634e989ae73f027
apek3 <apek3@gatech.edu>
2024-04-21 12:59:47 -0400

    Added WebRTC functionality

--- network/receiver.py
+++ network/receiver.py
@@ -2,16 +2,21 @@
 
 import socket
 import requests
+import struct
+
 
 # Set up the UDP socket
 UDP_IP = "0.0.0.0"  # Listen on all available network interfaces
 UDP_PORT = 5005
 
 # Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
 # Bind the socket to the port
-sock.bind((UDP_IP, UDP_PORT))
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+sock.bind(('', UDP_PORT))
+
 
 print("Listening for broadcast messages...")
 
@@ -20,9 +25,17 @@ try:
     while True:
         data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
         payload = {'Game': data.decode()}
-        r = requests.post('http://localhost:5000/findNodes', json=payload)
+        message = data.decode()
+        print("Received message:", message)
+        if(message.find("Remove -") != -1):
+            messageArray = message.split('-')
+            print(messageArray)
+            payload = {'Game': messageArray[1]}
+            #Need to remove this node from other flask servers that peers are running, still needs to be implemented
+            r = requests.post('http://localhost:5000/removeNode', json=payload)
+        else:
+            r = requests.post('http://localhost:5000/findNodes', json=payload)
         #print(r.text)
-        print("Received message:", data.decode())
 
 finally:
     sock.close()
